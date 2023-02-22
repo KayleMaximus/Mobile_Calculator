792fe2617d2f64ab2c7a9d622d03e036a2651266
Manh-Huy <tranmanhhuy2002@gmail.com>
2023-02-22 21:29:01 +0700

    tạo funct stop

--- app/src/main/java/hcmute/edu/vn/calulator_mobile/MainActivity.java
+++ app/src/main/java/hcmute/edu/vn/calulator_mobile/MainActivity.java
@@ -11,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void onStop() {
+        super.onStop();
+        setContentView(R.layout.activity_main);
+    }
 }
